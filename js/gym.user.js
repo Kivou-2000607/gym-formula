fdcf660535ec64dabdaa40e236d57d2dbba2161e
44345987+TotallyNot@users.noreply.github.com <44345987+TotallyNot@users.noreply.github.com>
2020-04-30 15:57:31 +0200

    bumped version 0.2

--- js/gym.user.js
+++ js/gym.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gym scraper
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Pyrit[2111649]
 // @match        https://www.torn.com/gym.php
